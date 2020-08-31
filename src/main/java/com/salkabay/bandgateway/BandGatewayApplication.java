94f226d3705192e6bcb7be1bae25626f2878235d
salska <sal@emetrix.biz>
2020-08-31 13:33:17 +0100

    Initial commit

---
+++ src/main/java/com/salkabay/bandgateway/BandGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.salkabay.bandgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BandGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BandGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/salkabay/bandgateway/BandGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salkabay.bandgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BandGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
